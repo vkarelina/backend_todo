fd832fad196733bfe9e6c71fcb1622aa6906d3d2
dunice0916 <dunice0916@dunice2s-Mac-mini.local>
2024-07-08 18:04:12 +0300

    update main.js

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = Number(process.env.PORT) | 5000;
+  const PORT = Number(process.env.PORT);
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
